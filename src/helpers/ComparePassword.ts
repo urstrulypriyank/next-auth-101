81d179a16ae45a7899d56f44df32582026a837a8
priyank rai <type2email@gmail.com>
2023-10-14 16:15:56 +0530

    bugfix

--- src/helpers/ComparePassword.ts
+++ src/helpers/ComparePassword.ts
@@ -1,5 +1,5 @@
-import bcryptjs from "bcryptjs"
-export async function Compare(password: string, UserPassword: string)
-{
-    const isPasswordSame = await bcryptjs.compare(password, UserPassword)
+import bcryptjs from "bcryptjs";
+export async function Compare(password: string, UserPassword: string) {
+  const isPasswordSame = await bcryptjs.compare(password, UserPassword);
+  return isPasswordSame;
 }
